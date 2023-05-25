6950773c8a7d4d1827a4d6f35ca9c6d3823bd9f0
slowandrarecooked <airbendervineeth@gmail.com>
2023-05-25 19:46:29 +0530

    styles added

--- cart.js
+++ cart.js
@@ -1,44 +1,65 @@
 var cartitems=JSON.parse(localStorage.getItem('cartitems'));
 var cart=document.querySelector('#cart');
+var promo=document.querySelector('#promo');
+var bt=document.querySelector('#bt');
+var emptycart=document.querySelector('#emptycart');
+var discountapplied=false;
+document.querySelector('#button-addon2').addEventListener('click',function(){
+    var discount=document.querySelector('#promocode').value;
+    applydiscount(discount);
+});
 var total=cartitems.reduce(function(acc,obj){
     return acc+obj.price*obj.item;
 },0);
-document.querySelector('#totalprice').textContent=total;
+calculate();
 generateCartItems();
 function generateCartItems(){
-    var total=cartitems.reduce(function(acc,obj){
-        return acc+obj.price*obj.item;
-    },0);
-    document.querySelector('#totalprice').textContent=total;
-    document.querySelector('#cart-size').textContent=cartitems.length;
-    return(cart.innerHTML=cartitems.map(function(data){
-        document.querySelector('#cart-size').textContent=cartitems.length;
-        
-        let{id,name,price,desc,img,item}=data;
-        return`
-        <div class="item">
-            <div>
-                <img src=${img} alt="">
-            </div>
-            <div>
+    if(cartitems.length!=0){
+        var total=cartitems.reduce(function(acc,obj){
+            return acc+obj.price*obj.item;
+        },0);
+        document.querySelector('#totalprice').textContent=total;
+        return(cart.innerHTML=cartitems.map(function(data){  
+            let{id,name,price,desc,img,item}=data;
+            return`
+                <div class="item">
                 <div>
-                    ${name}
+                    <div class="img">
+                        <img src="${img}" alt="">
+                    </div>
+                    <div class="name">
+                        <p>${name}</p>
+                    </div>
+                    <div class="delete">
+                        <i class="bi bi-trash" onclick='remove(${id})'></i>
+                    </div>
                 </div>
+                <hr>
                 <div>
-                    <i onclick='decrease(${id})' class="bi bi-dash-lg"></i>
-                    <div id=${id} class="quantity">${item}</div>
-                    <i onclick='increase(${id})' class="bi bi-plus-lg"></i>
-                </div>
-                <div>
-                    <p id='#price'>Rs ${price}</p>
+                    <div>
+                        <p>Quantity:</p>
+                        <i class="bi bi-dash" onclick='decrease(${id})'></i>
+                        <p id='${id}'>${item}</p>
+                        <i class="bi bi-plus" onclick='increase(${id})'></i>
+                    </div>
+                    <div>
+                        <b>₹<b id='price'>${price*item}</b></b>
+                    </div>
                 </div>
             </div>
-            <div id="#remove" onclick='remove(${id})'>
-                <i class="bi bi-x-lg"></i>
-            </div>
-        </div> 
-        `
-    }).join(" "));
+            `
+        }).join(" "));
+    }else{
+        cart.innerHTML="";
+        promo.innerHTML="";
+        bt.innerHTML="";
+        emptycart.innerHTML=
+            `<img src="images/empty-cart-4816550-4004141.png" alt="">
+            <p>Your Shopping Bag is empty</p>
+            <p>This feels too light,Go on, add all your favourites</p>
+            <button type="button" class="btn btn-primary">Start Shopping</button>`
+    }
+    
 }
 function increase(id){
     var target=cartitems.find(function(data){
@@ -74,14 +95,25 @@ function calculate(){
     },0)
     document.querySelector('#cart-size').textContent=totalqty;
     document.querySelector('#totalprice').textContent=total;
+    generateCartItems();
 }
 function remove(id){
     cartitems.map(function(data,i){
         if(data.id===id.id){
             cartitems.splice(i,1);
             localStorage.setItem('cartitems',JSON.stringify(cartitems));
+            calculate();
             generateCartItems();
             return;
         }
     })  
+}
+function applydiscount(discount){
+    if(discount==='masai30'){
+        discountapplied=true;
+        total-=total*0.3;
+        document.querySelector('#totalprice').textContent=total;
+    }else{
+        alert('Promo code not found')
+    }
 }
