4ae1203ec51786a181e9ae073760e15778768648
anshulgusain <anshulgusain99@gmail.com>
2023-06-02 10:04:41 +0530

    folders added

---
+++ Scripts/login.js
@@ -0,0 +1,25 @@
+var loginarr=JSON.parse(localStorage.getItem("credentials"))||[]
+document.querySelector("form").addEventListener("submit",check)
+var flag=0;
+function check(){
+    event.preventDefault();
+  var mobile=document.getElementById("number").value;
+  var password=document.getElementById("number2").value;
+ // console.log(mobile)
+  loginarr.map(function(ele){
+      
+      if((mobile===ele.mobile ||mobile===ele.email) && (password===ele.password)){
+        alert("Welcome Back Nykaa User")
+        flag=1;
+      }
+     
+  })
+  if(flag==0){
+  alert("Please sign up first")
+  signup()
+  }
+}
+ function signup(){
+  window.location.href = "signup.html";
+ }
+ 
