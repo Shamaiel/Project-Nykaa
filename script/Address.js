bea8f6882e96ac5fbf6569ad6bfdd715b370c12a
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2023-06-02 22:19:21 +0530

    add in file

--- script/Address.js
+++ script/Address.js
@@ -5,9 +5,9 @@ function Address() {
  <body id="body1">
     <div class="store1">
         <p>Address</p>
-        <p> <input type="number" class="Main" placeholder="Pincode"></p>
-        <p><input type="textarea" class="Main" placeholder="House/Flate/Office No"></p>
-        <p><input type="textarea" class="Main" placeholder="Road Name/ Area/ Colony"></p>
+        <p> <input type="number" class="Main" placeholder="Pincode"required></p>
+        <p><input type="textarea" class="Main" placeholder="House/Flate/Office No"required></p>
+        <p><input type="textarea" class="Main" placeholder="Road Name/ Area/ Colony"required></p>
     </div>
     <div class="store1">
         <p>
@@ -15,8 +15,8 @@ function Address() {
         </p>
         <p>Information provided here will be used to contact you for delivery updates</p>
        
-        <p> <input type="textarea" class="Main" placeholder="name"></p>
-        <p><input type="number" class="Main" placeholder="phone number"></p>
+        <p> <input type="textarea" class="Main" placeholder="name"required></p>
+        <p><input type="number" class="Main" placeholder="phone number"required></p>
         <p><input type="textarea" class="Main" placeholder="email id"></p>
     </div>
     <a href="payment.html">
